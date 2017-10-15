90a45af9c364f3905401f7e782e369f086a1a29b
robdir <rhwickham@gmail.com>
2017-10-15 18:09:01 +0200

    Deploy Heroku

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Artist, type: :model do
     it { is_expected.to validate_presence_of(:name)}
     it { is_expected.to validate_length_of(:bio).is_at_most(500) }
   end
+
+
+  describe "association with song"
+    let(:artist) { create :artist }
+    let!(:song)  { create :song, artist: artist}
+
+    it "has many songs" do
+      song1 = artist.songs.new(name: "No Hope")
+      song2 = artist.songs.new(name: "All Day Parade")
+
+      expect(artist.songs).to include(song1)
+      expect(artist.songs).to include(song2)
+
+    end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Song, type: :model do
       expect(song.errors).to have_key(:name)
     end
   end
+
+  describe "association with songs" do
+    let(:song) { create :song}
+    it { is_expected.to belong_to :artist}
+  end
 end
