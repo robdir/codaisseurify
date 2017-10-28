e68d8737b49c6befc6f55d8566f54b7dcfaf7ba4
robdir <rhwickham@gmail.com>
2017-10-28 22:19:57 +0200

    Set-up for full-stack testing

--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     name    { Faker::Name.first_name }
     formed  2015
     bio     { Faker::Lorem.sentence(2)}
+    id      {1}
   end
 end
--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :song do
     name  { Faker::Name.first_name }
+    artist_id { build(:artist)}
   end
 end
